df769b22fda67388432f726d9178c5df8e83f7db
Clément DOUIN <soywod@users.noreply.github.com>
2019-11-19 18:53:16 +0100

    add CSS preprocessor

--- src/Home/View.hs
+++ src/Home/View.hs
@@ -2,13 +2,26 @@
 
 module Home.View where
 
-import           Web.Scotty                    as Scotty
-import           Text.Blaze.Html5              as Html
-import           Text.Blaze.Html5.Attributes
+import qualified Web.Scotty                    as Scotty
+import           Web.Scotty                     ( ActionM )
+import qualified Text.Blaze.Html5              as Html
+import           Text.Blaze.Html5               ( (!) )
+import qualified Text.Blaze.Html5.Attributes   as Attr
+import qualified Clay                          as Style
+import           Clay                           ( Css
+                                                , (?)
+                                                )
 import           Text.Blaze.Html.Renderer.Utf8
 import           Data.Text.Lazy.Encoding
 
 render :: ActionM ()
 render = Scotty.html $ decodeUtf8 $ renderHtml $ do
-  Html.head $ Html.title "Home"
-  Html.body $ p "Home"
+  Html.head $ do
+    Html.title "Home"
+    Html.style $ Html.toHtml $ Style.renderWith Style.compact [] style
+  Html.body $ do
+    Html.div "Home" ! Attr.class_ "title"
+
+style :: Css
+style = ".title" ? do
+  Style.color "#ff0000"
